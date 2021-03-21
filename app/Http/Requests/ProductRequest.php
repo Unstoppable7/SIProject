89daa8b5b51a8412415f90152f580dacf758a618
Cristian Rosales <alejandro7120@gmail.com>
2021-03-21 19:40:46 -0400

    update little somthings

--- app/Http/Requests/ProductRequest.php
+++ app/Http/Requests/ProductRequest.php
@@ -25,7 +25,7 @@ class ProductRequest extends FormRequest
     {
         return [
             'name' => 'required',
-            //'selection' => 'required'
+            'selection' => 'required'
         ];
     }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Audit::class, 31)->create();
+        //factory(App\Audit::class, 31)->create();
 
         App\User::create([
             'name' => 'Cristian Rosales',
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
             'password' => bcrypt('admin')
         ]);
 
-        factory(App\Company::class, 10)->create();
+        //factory(App\Company::class, 10)->create();
         //factory(App\Product::class, 20)->create();
     }
 }
